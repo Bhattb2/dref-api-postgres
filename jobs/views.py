a04cd5b0b88f9bce69220bf07a53198c2c88e685
Bhagirath Bhatt <budbhatt@gmail.com>
2021-03-23 18:01:35 -0700

    lab needs more work

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+from .models import Job
+from .permissions import IsAuthOrReadOnly
+from .serializers import JobSerializer
+
+
+class JobList(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+
+class JobDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthOrReadOnly,)
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
